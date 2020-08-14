45086dd537571ffbd0f534fe095410cd58d48f8d
Sonal Singh <sonal.singh.19993@gmail.com>
2020-08-14 19:51:15 +0530

    sm6250-common: lineage -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf hardware/qcom-caf/sm8150
+git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-12
