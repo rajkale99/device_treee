24e0073ee97f0510f6145a6568bd75a4d6f2a6c3
lucky <ah440099@gmail.com>
2020-09-16 10:48:49 +0200

    RMX1971: Remove KharaMeParts

--- device.mk
+++ device.mk
@@ -215,10 +215,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
-# Doze
-PRODUCT_PACKAGES += \
-    KharaMeParts
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.oppo.hardware.biometrics.fingerprint@2.1 \
