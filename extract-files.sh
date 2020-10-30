d90de0be9713c9c4a1f21d849e90eaa27e9535d9
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-30 15:04:45 +0200

    sm6250-common: switch to lineage patchelf

--- extract-files.sh
+++ extract-files.sh
@@ -55,10 +55,10 @@ function blob_fixup() {
         patchelf --add-needed "libcutils_shim.so" "${2}"
         ;;
     vendor/lib64/hw/camera.qcom.so)
-        patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
-        patchelf --remove-needed "libmegface.so" "${2}"
-        patchelf --add-needed "libc++demangle.so" "${2}"
-        patchelf --add-needed "libshim_megvii.so" "${2}"
+        $PATCHELF --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+        $PATCHELF --remove-needed "libmegface.so" "${2}"
+        $PATCHELF --add-needed "libc++demangle.so" "${2}"
+        $PATCHELF --add-needed "libshim_megvii.so" "${2}"
         ;;
     esac
 }
