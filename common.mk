2b58cb618e86e45090359eeb422fb7afdb745f6b
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-25 15:27:55 +0000

    sm6250: Import vendor/qcom/opensource/commonsys/packages/apps/Bluetooth soong namespace

--- common.mk
+++ common.mk
@@ -359,7 +359,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/xiaomi  \
+    vendor/nxp/opensource/sn100x \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 # Telephony
 PRODUCT_PACKAGES += \
